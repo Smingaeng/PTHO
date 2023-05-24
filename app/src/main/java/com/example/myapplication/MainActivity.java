dbdbbe76639eade55c1c668c43f78f16ac660b6c
minkyung <xmskwo349@naver.com>
2023-05-24 14:31:09 +0900

    5/24 Commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,19 +14,117 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b1 = (Button) findViewById(R.id.appCompatButton);
-        b1.setOnClickListener(new View.OnClickListener() {
+        Button appCompatButton = findViewById(R.id.appCompatButton);
+        Button appCompatButton2 = findViewById(R.id.appCompatButton2);
+        Button appCompatButton3 = findViewById(R.id.appCompatButton3);
+        Button appCompatButton5 = findViewById(R.id.appCompatButton5);
+        Button appCompatButton7 = findViewById(R.id.appCompatButton7);
+        Button appCompatButton8 = findViewById(R.id.appCompatButton8);
+        Button appCompatButton9 = findViewById(R.id.appCompatButton9);
+        Button appCompatButton10 = findViewById(R.id.appCompatButton10);
+        Button appCompatButton11 = findViewById(R.id.appCompatButton11);
+        Button appCompatButton12 = findViewById(R.id.appCompatButton12);
+        Button appCompatButton16 = findViewById(R.id.appCompatButton16);
+        Button appCompatButton14 = findViewById(R.id.appCompatButton14);
+        Button appCompatButton15 = findViewById(R.id.appCompatButton15);
+        Button appCompatButton17 = findViewById(R.id.appCompatButton17);
+
+        appCompatButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                b1.setSelected(true);
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton.setSelected(true);
             }
         });
-
-        Button b2 = (Button) findViewById(R.id.appCompatButton2);
-        b2.setOnClickListener(new View.OnClickListener() {
+        appCompatButton2.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                b2.setSelected(true);
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton2.setSelected(true);
+            }
+        });
+        appCompatButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton3.setSelected(true);
+            }
+        });
+        appCompatButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton5.setSelected(true);
+            }
+        });
+        appCompatButton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton7.setSelected(true);
+            }
+        });
+        appCompatButton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton8.setSelected(true);
+            }
+        });
+        appCompatButton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton9.setSelected(true);
+            }
+        });
+        appCompatButton10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton10.setSelected(true);
+            }
+        });
+        appCompatButton11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton11.setSelected(true);
+            }
+        });
+        appCompatButton12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton12.setSelected(true);
+            }
+        });
+        appCompatButton14.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton14.setSelected(true);
+            }
+        });
+        appCompatButton15.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton15.setSelected(true);
+            }
+        });
+        appCompatButton16.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton16.setSelected(true);
+            }
+        });
+        appCompatButton17.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼이 클릭되었을 때 실행될 코드 작성
+                appCompatButton17.setSelected(true);
             }
         });
 
